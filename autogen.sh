fc1e6f412698c7acbe8da0e88fb88987dce254d5
System Administrator <root@eric-gallagers-macbook-pro.local>
2013-06-01 16:32:42 -0400

    fiddle around with build system

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+echo "It'd probably be a better idea to just run \`autoreconf' (with whichever flags you choose) instead of running this script, but whatever..."
+
 AUTOGEN_DIR=dists/autogen
 aclocal -I ${AUTOGEN_DIR}/m4 && autoconf
---
+++ ppaslink.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DoExitAsm ()
+{ echo "An error occurred while assembling $1"; exit 1; }
+DoExitLink ()
+{ echo "An error occurred while linking $1"; exit 1; }
+OFS=$IFS
+IFS="
+"
+/opt/local/bin/ld /usr/lib/crt1.o  -z noexecstack     -multiply_defined suppress -L. -o conftest `cat link.res` -pagezero_size 0x10000
+if [ $? != 0 ]; then DoExitLink ; fi
+IFS=$OFS
